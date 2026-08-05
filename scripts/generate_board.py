491186b1ecedd7cc1cc18b79c1edc5b5d199b358
Caarda <caarda2@gmail.com>
2026-08-06 05:55:06 +1000

    added board generation and changed categories.csv structure

---
+++ scripts/generate_board.py
@@ -0,0 +1,107 @@
+# Takes runs.csv and categories.csv files and generates the full board.csv that gets written to sheets.
+
+import csv, math
+
+def FormatTime(time: float):
+    ms, ss = math.modf(time)
+    hh, ss = divmod(ss, 3600)
+    mm, ss = divmod(ss, 60)
+
+    # Show existing ms up to three decimal places and pad seconds if needed
+    padding = "0" if 0 < ss < 10 else "0" if ss == 0 and mm else ""
+    ss = f"{padding}{ss + round(ms, 3)}".rstrip("0").rstrip(".")
+
+    if hh:
+        return f"{int(hh)}:{int(mm):02d}:{ss}"
+    if mm:
+        return f"{int(mm)}:{ss}"
+    else:
+        return f"{ss}"
+
+with open("output/categories.csv", "r", encoding="utf-8") as CategoriesFile:
+    with open("output/runs.csv", "r", encoding="utf-8") as RunsFile:
+        CategoriesReader = csv.reader(CategoriesFile)
+        RunsReader = csv.reader(RunsFile)
+        next(CategoriesReader)
+        next(RunsReader)
+
+        # We build a dict of dicts that accepts keys [Player][CategoryID] and returns the time of their run if it exists, else 0.
+        # We also keep track of the World Record in each category for point calculations.
+        Players = dict()
+        Categories = dict()
+
+        for CategoryID, MaxPoints, _, Game, Category, Subcategory in CategoriesReader:
+            Categories[CategoryID] = [0.0, int(MaxPoints), Game.lstrip(" "), Category, Subcategory]
+
+        for run in RunsReader:
+            _, _, _, CategoryID, PlayerName, RunTime, _, _, _ = run
+
+            if not PlayerName in Players.keys():
+                Players[PlayerName] = {"Rank": 0, "Total": 0, "Name": PlayerName}
+                for Category in Categories.keys(): Players[PlayerName][Category] = 0
+
+            RunTime = float(RunTime)
+            Players[PlayerName][CategoryID] = float(RunTime)
+
+            if RunTime < Categories[CategoryID][0] or not Categories[CategoryID][0]:
+                Categories[CategoryID][0] = RunTime
+
+        # Times can now be converted into points. Current formula is simply round(MAX_POINTS * (WR / TIME)).
+        for Player in Players.keys():
+
+            for Category in Categories.keys():
+                if Players[Player][Category]:
+                    PlayerScore = round(Categories[Category][1] * (Categories[Category][0] / Players[Player][Category]) ** 2)
+                    Players[Player][Category] = f"{FormatTime(Players[Player][Category])} ({PlayerScore})"
+                    Players[Player]["Total"] += PlayerScore
+                else:
+                    Players[Player][Category] = ""
+
+# Now we can sequentially assign ranks in descending score order.
+# In the case of ties, players get assigned the same rank and we skip one number forwards.
+Previous = 0
+Rank = 0
+Tie = 0
+
+with open("output/board.csv", "w", newline="", encoding="utf-8") as BoardFile:
+    BoardWriter = csv.writer(BoardFile)
+
+    # We need to structure the three top rows column by column for sheet styling to work properly.
+    BoardGameHeader = ["Rank", "Total Score", "Player"]
+    BoardCategoryHeader = ["", "", ""]
+    BoardSubcategoryHeader = ["", "", ""]
+
+    LastGame = ""
+    LastCategory = ""
+
+    for CategoryID in Categories.keys():
+        Game, Category, Subcategory = Categories[CategoryID][2], Categories[CategoryID][3], Categories[CategoryID][4]
+
+        if Game != LastGame:
+            BoardGameHeader.append(Game)
+            LastGame = Game
+        else: BoardGameHeader.append("")
+
+        if Category != LastCategory:
+            BoardCategoryHeader.append(Category)
+            LastCategory = Category
+        else: BoardCategoryHeader.append("")
+
+        if Subcategory: BoardSubcategoryHeader.append(Subcategory)
+
+    BoardWriter.writerow(BoardGameHeader)
+    BoardWriter.writerow(BoardCategoryHeader)
+    BoardWriter.writerow(BoardSubcategoryHeader)
+
+    # Finally, we write player rows in order.
+    for Player in sorted(Players, key=lambda player: Players[player]["Total"], reverse=True):
+        if Players[Player]["Total"] == Previous:
+            Tie += 1
+        else:
+            Rank += Tie + 1
+            Tie = 0
+
+        Players[Player]["Rank"] = Rank
+        PlayerRow = list(Players[Player].values())
+
+        BoardWriter.writerow(PlayerRow)
--- scripts/update_category_values.py
+++ scripts/update_category_values.py
@@ -1,8 +1,8 @@
-# Calculates category point values based on the number of runs and writes them to categories.csv
+# Calculates category point values based on the number of runs and writes them to categories.csv.
 
 import csv, math
 
-# Count the number of runs in each category
+# Count the number of runs in each category.
 with open("output/runs.csv", "r", encoding="utf-8") as f:
     reader = csv.reader(f)
     header = next(reader)
@@ -12,19 +12,24 @@ with open("output/runs.csv", "r", encoding="utf-8") as f:
     for run in reader:
         GameName, CategoryName, SubcategoryName, CategoryID, PlayerName, RunTime, RunDate, Platform, RunID = run
 
-        # Add unseen categories
+        # Add unseen categories.
         if CategoryID not in RunnerCounts:
-            ReadableCategoryName = f"{GameName} - {CategoryName} ({SubcategoryName})" if SubcategoryName else f"{GameName} - {CategoryName}"
-            RunnerCounts[CategoryID] = [ReadableCategoryName, 1]
+            RunnerCounts[CategoryID] = [1, GameName, CategoryName, SubcategoryName]
         else:
-            RunnerCounts[CategoryID][1] += 1
+            RunnerCounts[CategoryID][0] += 1
 
-# Calculate values and write to file
+# Calculate values and write to file.
 with open("output/categories.csv", "w", newline="", encoding="utf-8") as f:
     f.write("category_id,points,category_name\n")
-    MaxRunnerCount = max(x[1] for x in RunnerCounts.values())
 
+    # The most runners in a single category.
+    HighestRunnerCount = max(x[0] for x in RunnerCounts.values())
+
+    # Max points for each category based on the number of runners. Current formula is [20 + round(80 * log(RUNNERS, HIGHEST_RUNNERS))].
     for CategoryID in RunnerCounts:
-        ReadableCategoryName, PlayerCount = RunnerCounts[CategoryID]
-        PointValue = 20 + round(80 * math.log(PlayerCount, MaxRunnerCount))
-        f.write(f"{f'{CategoryID},':<27}{f'{PointValue},':<6}// {ReadableCategoryName}\n")
+        PlayerCount, GameName, CategoryName, SubcategoryName = RunnerCounts[CategoryID]
+        ReadableCategoryName = f"{GameName} - {CategoryName} ({SubcategoryName})" if SubcategoryName else f"{GameName} - {CategoryName}"
+
+        PointValue = 20 + round(80 * math.log(PlayerCount, HighestRunnerCount))
+
+        f.write(f"{f'{CategoryID},':<27}{f'{PointValue},':<6}{f' | {ReadableCategoryName},':<80}{f'{GameName},'}{f'{CategoryName},'}{f'{SubcategoryName}'}\n")
